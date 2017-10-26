4cd6de7caaae86e0ba326e68a9156d5369ac06ad
IT-Department <IT-Department@IT-DEPARTMENT-P>
2017-10-26 12:52:29 +0330

    firstUpload

---
+++ sliding_Menu/Controllers/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace sliding_Menu.Controllers
+{
+    [Activity(Label = "Main", MainLauncher = true/*,Theme =""*/)]
+    public class Main : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
